8b287c75f0786d85a874314fddeee8a5e6819c62
anshumanrai433 <anshuman.23scse1012217@galgotiasuniversity.ac.in>
2024-12-24 14:09:18 +0530

    initial commit
    Signed-off-by: anshumanrai433 <anshuman.23scse1012217@galgotiasuniversity.ac.in>

---
+++ javascript/Validate.js
@@ -0,0 +1,14 @@
+function validateForm() {
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    if (username.length < 3) {
+        alert("Username must be at least 3 characters long.");
+        return false;
+    }
+    if (password.length < 6) {
+        alert("Password must be at least 6 characters long.");
+        return false;
+    }
+    return true;
+}
